f628561a98c7720fa48dd58a92e57e5a4b559d8b
Daniel da Costa <danielcostacct@gmail.com>
2016-02-25 11:05:30 +0000

    resposta a pergunta secreta

--- socket-client/src/main/java/example/SocketClient.java
+++ socket-client/src/main/java/example/SocketClient.java
@@ -1,6 +1,9 @@
 package example;
 
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -35,13 +38,37 @@ public class SocketClient {
         // Create stream to send data to server
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-        // Send text to server as bytes
-        out.writeBytes(text);
+    	// Obtem o tempo em segundos
+        long tempoActual = System.currentTimeMillis();
+        System.out.println("Tempo do Cliente: " + tempoActual);
+                        
+        out.writeBytes(Long.toString(tempoActual));
         out.writeBytes("\n");
-        System.out.println("Sent text: " + text);
+        
+        
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        BufferedReader d = new BufferedReader(new InputStreamReader(in));
+        
+        //recebe a resposta
+        String resposta;
+        resposta = d.readLine();
+        System.out.println("Recebemos a resposta do servidor: " + resposta);
+        
+        if(Integer.parseInt(resposta) == 0){
+	        // Send text to server as bytes
+	        out.writeBytes(text);
+	        out.writeBytes("\n");
+	        
+	        System.out.println("Sent text: " + text);
+        }else{
+	        System.out.println("A timestamp do cliente em relação ao servidor, difere em mais de 50ms, por favor sincronize os relógios \n");
+
+        	
+        }
 
         // Close client socket
         socket.close();
         System.out.println("Connection closed");
     }
 }
+
