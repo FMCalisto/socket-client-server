b8505a4ba30fbe01cc9882ddd284b7246ae7fe4d
Francisco Maria Calisto <francisco.mcalisto@gmail.com>
2016-02-26 23:02:54 +0000

    [UPDATE] Refresh Files

--- socket-server/src/main/java/example/SocketServer.java
+++ socket-server/src/main/java/example/SocketServer.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.DataOutputStream;
 
 
 public class SocketServer 
@@ -29,46 +28,16 @@ public class SocketServer
         // a socket is created to handle the created connection
         Socket clientSocket = serverSocket.accept();
         System.out.printf("Connected to client %s on port %d %n",
-        clientSocket.getInetAddress().getHostAddress(), clientSocket.getPort());
+            clientSocket.getInetAddress().getHostAddress(), clientSocket.getPort());
 
         // Create stream to receive data from client
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        
-        //Obtem o tempo do cliente, como string
-        String tempoDoClienteString;
-        tempoDoClienteString = in.readLine();
-        
-        //Obtem o tempo do servidor
-        long tempoDoServidor = System.currentTimeMillis();
-                
-        System.out.println("O tempo recebido: " + tempoDoClienteString + "\n" + "O tempo do servidor: " + tempoDoServidor + "\n");
-        
-        //Converte o tempo do cliente, de string para long.
-        //Faco isto apos obter o tempo do servidor, para que caso a conversao demore muito tempo, esta nao afecte o tempo obtido no servidor
-        long tempoDoCliente = Long.parseLong(tempoDoClienteString);
-        
-        //Abre o canal para responder ao cliente
-        DataOutputStream outc = new DataOutputStream( clientSocket.getOutputStream() );
 
-    	tempoDoServidor = tempoDoServidor + 50;
-    	
-        if( (tempoDoCliente < (tempoDoServidor + 50)) || (tempoDoCliente > (tempoDoServidor - 50) )){
-        	//Resposta     
-            outc.writeBytes("0\n");	        	
-        }else{
-        	//Resposta
-        	outc.writeBytes("-1\n");        	
-        }
-        
         // Receive data until client closes the connection
         String response;
-        response = in.readLine();
-        
-        //  while ((response = in.readLine()) )//!= null)
-        System.out.printf("Received message with content: '%s'%n", response);
-      
-       
-        
+        while ((response = in.readLine()) != null)
+            System.out.printf("Received message with content: '%s'%n", response);
+
         // Close connection to current client
         clientSocket.close();
         System.out.println("Closed connection with client");
@@ -78,4 +47,3 @@ public class SocketServer
         System.out.println("Closed server socket");
     }
 }
-
